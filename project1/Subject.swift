9411975aeb11e86fb5718ef2de64af7991f51ac5
ErykBrol <brol.eryk@gmail.com>
2019-05-09 17:47:49 -0400

    Initial commit

---
+++ project1/Subject.swift
@@ -0,0 +1,22 @@
+//
+//  subject.swift
+//  project1
+//
+//  Created by Budding Minds Admin on 2018-09-05.
+//  Copyright © 2018 The Budding Minds Lab. All rights reserved.
+//
+
+import Foundation
+
+class Subject {
+    var subjectNum:String? = nil
+    var learningRate:[String]? = nil
+    var responseStats:[String]? = nil
+    
+    init(subjectNum:String) {
+        self.subjectNum = subjectNum
+    }
+    
+    
+    
+}
